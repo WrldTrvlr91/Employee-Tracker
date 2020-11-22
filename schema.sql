e7279752584a92ba6d522a837c1f63770221cbc0
Rob Sonter <rob.sonter@newstartdigital.com.au>
2020-11-22 17:23:09 +0800

    Udpated SQL Query and layout of command line

--- schema.sql
+++ schema.sql
@@ -3,15 +3,6 @@ CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
 
-CREATE TABLE employee(
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE department(
     id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
@@ -23,6 +14,17 @@ CREATE TABLE role(
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)    
+);
